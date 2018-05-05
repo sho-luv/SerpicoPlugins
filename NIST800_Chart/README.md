238c4dd5fa6634a03f63336a70d27a1ece562c8d
sho-luv <sho.luv@gmail.com>
2018-05-05 04:34:46 -0700

    added NIST800 Chart plugin

---
+++ NIST800_Chart/README.md
@@ -0,0 +1,11 @@
+# Overview
+
+This plug-in adds CIS Top 20 Controls to the findings database. This will allow each finding to have CIS control assigned to it by adding a cis_control variable to each finding in the database. Since there are 20 CIS controls they will be presented as a drop down menu option when editing or adding findings.
+
+# Command Line
+
+The plugin can be run from the command line in the Serpico directory.
+
+```
+cd plugins/CIS_Control
+```
---
+++ NIST800_Chart/nist800-30_chart_add.rb
@@ -0,0 +1,42 @@
+if File.file?("#{Dir.pwd()}/plugins/NIST800_Chart/installed")
+
+    text_to_replace = '    - elsif @nist800
+      .control-group.nist800'
+
+    new_text = '    - elsif @nist800
+      .control-group.nist800
+        %label.control-label
+        .controls
+          %a.btn.btn-info{ :href=> "#nist800modal", "data-toggle" => "modal" }
+            NIST800-30 Chart
+        .modal.modal.hide.fade#nist800modal{ :tabindex => "-1", :role => "dialog", "aria-labelledby" => "modal-label", "aria-hidden" => "true"}
+          .modal-header
+            %button.close{ :type => "button", "data-dismiss" => "modal", "aria-hidden" => "true" }
+              X
+            %h3#modal-label
+              NIST800-30 Chart
+          .modal-body
+            %p
+              %img{:src => "/img/nist800.png"}'
+
+    file_names = ['views/create_finding.haml', 'views/findings_edit.haml']
+
+    file_names.each do |file_name|
+      text = File.read(file_name)
+      new_contents = text.gsub(/text_to_replace/, new_text)
+
+      # To merely print the contents of the file, use:
+      puts new_contents
+
+      # To write changes to the file, use:
+      #File.open(file_name, "w") {|file| file.puts new_contents }
+      File.write(file_name, text.gsub(/regexp/, "replace")
+    end
+    if File.file?("#{Dir.pwd()}/plugins/NIST800_Chart/nist800.png")
+      File.rename("#{Dir.pwd()}/plugins/NIST800_Chart/nist800.png", "#{Dir.pwd()}/public/img/nist800.png")
+    else
+      puts "|!| Failed to locate NIST800 image to use."
+
+else
+	puts "|!| Failed to load NIST800_Chart, see the README for installation instructions."
+end
